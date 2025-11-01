454baa1738c226939c1c499c5f97682ffe629df6
gabrielgua <gabrieljmym13@gmail.com>
2025-11-01 00:42:15 -0300

    feat: add Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,27 +1,31 @@
 <script setup lang="ts">
 import Container from './Container.vue';
+import LanguageSelect from './LanguageSelect.vue';
 import Logo from './Logo.vue';
 
 </script>
 
 <template>
-  <header class="bg-slate-900">
+  <header class="bg-slate-50 shadow-md">
     <Container class="flex items-center justify-between py-4 md:py-8 transition-all">
       <RouterLink to="/">
         <Logo />
       </RouterLink>
-      <ul class="flex items-center gap-6">
+      <ul class="flex items-center gap-6 text-sm font-medium">
         <li>
-          <RouterLink to="/" class="text-slate-300 hover:text-slate-100 transition-colors">Home</RouterLink>
+          <RouterLink to="/" class="text-slate-900 hover:text-sky-600 transition-colors">Home</RouterLink>
         </li>
         <li>
-          <RouterLink to="/about" class="text-slate-300 hover:text-slate-100 transition-colors">About Us</RouterLink>
+          <RouterLink to="/about" class="text-slate-900 hover:text-sky-600 transition-colors">About Us</RouterLink>
         </li>
         <li>
-          <RouterLink to="/services" class="text-slate-300 hover:text-slate-100 transition-colors">Services</RouterLink>
+          <RouterLink to="/services" class="text-slate-900 hover:text-sky-600 transition-colors">Services</RouterLink>
         </li>
         <li>
-          <RouterLink to="/contact" class="text-slate-300 hover:text-slate-100 transition-colors">Contact</RouterLink>
+          <RouterLink to="/contact" class="text-slate-900 hover:text-sky-600 transition-colors">Contact</RouterLink>
+        </li>
+        <li>
+          <LanguageSelect />
         </li>
       </ul>
     </Container>
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex items-center gap-2 text-2xl text-slate-200">
-    <img src="/src/assets/images/wuzimed_cross.png" alt="Wuzi Med Logo" class="w-[30px]" />
+  <div class="flex items-center gap-2 text-xl md:text-2xl transition-all text-primary-950">
+    <img src="/src/assets/images/wuzimed_cross.png" alt="Wuzi Med Logo" class="w-[25px]" />
     <p class="momo-font">
       <span class="font-bold">WUZI</span>
-      <span class="text-blue-300">med</span>
-      <span>.</span>
+      <span class="text-primary-300">med</span>
+      <!-- <span>.</span> -->
     </p>
   </div>
 </template>
