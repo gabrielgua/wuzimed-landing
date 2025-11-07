8025f93a172dd00a5b1891182d57fa086cb0859c
gabrielgua <gabrieljmym13@gmail.com>
2025-11-07 15:34:37 -0300

    chore: change header links decoration

---
+++ src/components/HeaderLink.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ to: string }>();
+</script>
+
+<template>
+  <RouterLink :to="to" active-class="font-bold underline decoration-2 text-primary-500" class="transition-all">
+    <slot />
+  </RouterLink>
+</template>
--- src/components/HeaderLinks.vue
+++ src/components/HeaderLinks.vue
@@ -1,27 +1,13 @@
 <script setup lang="ts">
-import Icon from './Icon.vue';
-import LanguageSelect from './LanguageSelect.vue';
+import HeaderLink from './HeaderLink.vue';
 
 </script>
 
 <template>
   <ul class="flex items-center gap-6 text-sm font-medium">
-    <li>
-      <RouterLink to="/" class="text-text-primary hover:text-primary-500 transition-colors">Home</RouterLink>
-    </li>
-    <li>
-      <RouterLink to="/about" class="text-text-primary hover:text-primary-500 transition-colors">About Us
-      </RouterLink>
-    </li>
-    <li>
-      <RouterLink to="/partners" class="text-text-primary hover:text-primary-500 transition-colors">
-        Partners
-      </RouterLink>
-    </li>
-    <li>
-      <RouterLink to="/contact" class="text-text-primary hover:text-primary-500 transition-colors">Contact
-      </RouterLink>
-    </li>
-
+    <HeaderLink to="/">Home</HeaderLink>
+    <HeaderLink to="/about">About Us</HeaderLink>
+    <HeaderLink to="/partners">Partners</HeaderLink>
+    <HeaderLink to="/contact">Contact</HeaderLink>
   </ul>
 </template>
