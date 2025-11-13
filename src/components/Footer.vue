5bbf38947b9e7008378c05a4631ad5c15e7fd8d2
gabrielgua <gabrieljmym13@gmail.com>
2025-11-13 15:22:28 -0300

    chore: localize footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,14 +12,14 @@ import Logo from './Logo.vue';
       <div class="space-y-4">
         <Logo variant="light" />
         <p class="text-secondary-200 text-sm font-light">
-          Welcome to WuziMed,
+          {{ $t('footerHeroText1') }}
           <br>
-          here to help you manage your health care needs!
+          {{ $t('footerHeroText2') }}
         </p>
 
       </div>
       <FooterSection>
-        <template #title>Socials</template>
+        <template #title>{{ $t('footerSocialsTitle') }}</template>
         <div class="flex items-center gap-2">
           <div class="size-10 rounded-xl bg-bg-base grid place-items-center text-text-primary">
             <Icon icon="fa-brands fa-instagram" class="text-2xl" />
@@ -36,33 +36,33 @@ import Logo from './Logo.vue';
         </div>
       </FooterSection>
       <FooterSection>
-        <template #title>Quick Links</template>
+        <template #title>{{ $t('footerQuickLinksTitle') }}</template>
         <div class="flex flex-col gap-2">
-          <RouterLink to="/" class="text-primary-500 text-sm font-medium">Home</RouterLink>
-          <RouterLink to="/" class="text-primary-500 text-sm font-medium">About us</RouterLink>
-          <RouterLink to="/" class="text-primary-500 text-sm font-medium">Partners</RouterLink>
-          <RouterLink to="/" class="text-primary-500 text-sm font-medium">Contact</RouterLink>
+          <RouterLink to="/" class="text-primary-500 text-sm font-medium">{{ $t('footerQuickLink1') }}</RouterLink>
+          <RouterLink to="/" class="text-primary-500 text-sm font-medium">{{ $t('footerQuickLink2') }}</RouterLink>
+          <RouterLink to="/" class="text-primary-500 text-sm font-medium">{{ $t('footerQuickLink3') }}</RouterLink>
+          <RouterLink to="/" class="text-primary-500 text-sm font-medium">{{ $t('footerQuickLink4') }}</RouterLink>
         </div>
       </FooterSection>
       <FooterSection>
-        <template #title>Contact</template>
+        <template #title>{{ $t('footerContactTitle') }}</template>
         <div class="font-medium text-sm text-bg-base">
           <p>+1 (12) 0345-6789</p>
           <p>support.help@wuzimed.com</p>
         </div>
       </FooterSection>
       <FooterSection>
-        <template #title>Location</template>
-        <p class="font-medium text-bg-base text-sm">Québec, CA</p>
+        <template #title>{{ $t('footerLocationTitle') }}</template>
+        <p class="font-medium text-bg-base text-sm">{{ $t('footerLocationAddressLine1') }}</p>
       </FooterSection>
     </Container>
     <Container class="py-6">
       <hr class="border-slate-800" />
     </Container>
     <Container class="flex items-center justify-between gap-6  text-xs text-bg-base">
-      <p>&copy; 2025 WuziMed. All rights reserved.</p>
+      <p>&copy; 2025 WuziMed. {{ $t('footerRightsReserved') }}</p>
       <p>
-        Develped by
+        {{ $t('footerDevelopedBy') }}
         <a href="https://github.com/gabrielgua" target="_blank" class="text-primary-600 hover:underline">
           @gabrielgua</a>
       </p>
