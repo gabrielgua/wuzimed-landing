9e5f51bf3bd2d5e8b5b388a23396a92927fa41af
gabrielgua <gabrieljmym13@gmail.com>
2025-11-13 16:30:21 -0300

    chore: add scrool to top whenever changin pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [{ path: '/', name: 'Home', component: () => import('@/views/HomeView.vue') }],
+  scrollBehavior() {
+    return { top: 0, behavior: 'smooth' }
+  },
 })
 
 export default router
